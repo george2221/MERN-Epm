06ec2c7e4a4c37d8940139f58a3b1a7afac5eb50
george richard <george.s@mphasis.com>
2023-12-21 22:29:31 +0530

    files

---
+++ backend/models/projectModel.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const projectsSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    client: {
+      type: String,
+      required: true,
+    },
+   
+    status: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Array,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Projects = mongoose.model("Projects", projectsSchema);
+
+export default Projects;
